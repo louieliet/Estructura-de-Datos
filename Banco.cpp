856f0776ed9c55d376f194c2af0f1820097577c3
louieliet <louieliet2003@gmail.com>
2022-09-05 15:41:37 -0500

    started list

---
+++ Banco.cpp
@@ -0,0 +1,405 @@
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <class T>
+class FiFo {
+protected:
+    T* aData;
+    int aMax;
+    int aCur;
+    int aErr;
+    int aStep;
+
+public:
+    FiFo(int pMax, int pStep = 0);
+    ~FiFo(void);
+
+    void push(T pVal);
+    T top(void);
+    void pop(void);
+    bool isEmpty(void);
+    bool isFull(void);
+
+    int error(void);
+    T get(int pIdx);
+    T operator [](int pIdx);
+    void repr(void);
+
+private:
+    void resize(void);
+};
+
+template <class T>
+FiFo<T>::FiFo(int pMax, int pStep)
+{
+    aCur = 0;
+    aErr = 0;
+    aData = NULL;
+    aStep = pStep;
+    aMax = 0;
+
+    if (pMax > 0) {
+        aMax = pMax;
+        aData = new T[aMax];
+        if (aData == NULL)
+            aErr = -2; // No hay memoria
+    }
+    else
+        aErr = -1; // Max fuera de rango
+} // Constructor
+
+template <class T>
+FiFo<T>::~FiFo(void)
+{
+    aErr = 0;
+    aMax = 0;
+    aCur = 0;
+
+    if (aData)
+        delete[] aData;
+}  // Destructor
+
+template <class T>
+void FiFo<T>::push(T pVal)
+{
+    aErr = 0;
+
+    if (aData) {
+        if (!isFull()) {
+            aData[aCur] = pVal;
+            aCur++;
+        }
+        else {
+            if (aStep != 0) {
+                resize();
+                if (aErr == 0) {
+                    aData[aCur] = pVal;
+                    aCur++;
+                }
+            } else
+                aErr = -3; // Intento de push en llena
+        }
+    }
+} // push
+
+template <class T>
+T FiFo<T>::top(void)
+{
+    aErr = 0;
+
+    if (aData) {
+        if (!isEmpty()) {
+            return aData[0];
+        }
+        else {
+            aErr = -4; // Intento de top en vac�a
+        }
+    }
+    return (T)0;
+} // top
+
+template <class T>
+void FiFo<T>::pop(void)
+{
+    aErr = 0;
+
+    if (aData) {
+        if (!isEmpty()) {
+            for(int lIdx = 0; lIdx < aMax - 1; lIdx++)
+            {
+                aData[lIdx] = aData[lIdx+1];
+            }
+            aCur--;
+        }
+        else {
+            aErr = -5; // Intento de top en vac�a
+        }
+    }
+} // pop
+
+template <class T>
+bool FiFo<T>::isEmpty(void)
+{
+    return (aCur == 0);
+} // isEmpty
+
+template <class T>
+bool FiFo<T>::isFull(void)
+{
+    return (aCur == aMax);
+} // isFull
+
+template <class T>
+int FiFo<T>::error(void)
+{
+    return aErr;
+} // error
+
+template <class T>
+T FiFo<T>::get(int pIdx)
+{
+    aErr = 0;
+
+    if (aData) {
+        if (!isEmpty()) {
+            if ((0 <= pIdx) && (pIdx < aCur)) {
+                return aData[pIdx];
+            }
+            else {
+                aErr = -7; // Intento de get fuera de rango
+            }
+        }
+        else {
+            aErr = -6; // Intento de get en vac�a
+        }
+    }
+    return (T)0;
+} // get
+
+template <class T>
+T FiFo<T>::operator [](int pIdx)
+{
+    return get(pIdx);
+} // operator []
+
+template <class T>
+void FiFo<T>::repr(void)
+{
+    cout << (isEmpty() == true ? "1" : "0");
+    cout << "|";
+    cout << (isFull() == true ? "1" : "0");
+    cout << "|";
+    cout << aMax;
+    cout << "|";
+    cout << aCur;
+    cout << "|";
+    cout << aErr;
+    cout << "||";
+    for (int lIdx = 0; lIdx < aCur; lIdx++) {
+        cout << aData[lIdx];
+        cout << ", ";
+    }
+    cout << endl;
+} // repr
+
+template <class T>
+void FiFo<T>::resize(void)
+{
+    int lMax = 0;
+    T* lData = NULL;
+
+    aErr = 0;
+
+    if (aData) {
+        if (aStep > 0) {
+            lMax = aMax + aStep;
+            lData = new T[lMax];
+            if (lData) {
+                for (int lIdx = 0; lIdx < aMax; lIdx++)
+                    lData[lIdx] = aData[lIdx];
+                delete[] aData;
+                aData = lData;
+                aMax = lMax;
+            }
+            else
+                aErr = -9; // No memoria para resize
+        }
+        else
+            aErr = -8; // Intento de resize fuera de rango
+    }
+} // resize
+
+
+#define K_MAX_Personas 1000
+#define K_MAX_Lugares 10
+#define K_MAX_Cajas 2
+#define K_MAX_CajaExclusive 1
+#define K_MAX_Opers 6
+
+
+int random(int pMin, int pMax, vector<int> v)
+{
+    return (v[rand() % (pMax - pMin) + pMin]);
+}
+
+
+vector<int> fillDistribution(vector<int> v)
+{
+    
+    vector<int> vtf = v;
+
+    for(int x = 0; x < 3; x++)
+    {
+        vtf.push_back(1);
+    }
+    for(int x = 0; x < 7; x++)
+    {
+        vtf.push_back(2);
+    }
+    for(int x = 0; x < 10; x++)
+    {
+        vtf.push_back(3);
+    }
+    for(int x = 0; x < 7; x++)
+    {
+        vtf.push_back(4);
+    }
+    
+    for(int x = 0; x < 4; x++)
+    {
+        vtf.push_back(5);
+    }
+        
+    vtf.push_back(6);
+
+    random_shuffle(vtf.begin(),vtf.end());
+
+    return vtf;
+
+}
+
+int main()
+{
+    FiFo<int> lCola = FiFo<int>(K_MAX_Lugares, 0);
+    FiFo<int> lColaExclusive = FiFo<int>(K_MAX_Lugares, 0);
+    int lCaja[K_MAX_Cajas];
+    int lCajaEx[K_MAX_CajaExclusive];
+    int lPersonas = 0;
+    int lCiclos = 0;
+    int lRebotes = 0;
+    int lOpers = 0;
+    bool lSalida = false;
+    int lCajaVacia = 0;
+    int lCajaExVacia = 0;
+
+    vector<int> distribution;
+    distribution = fillDistribution(distribution);
+
+    for(int x = 0; x < K_MAX_Cajas; x++)
+    {
+        lCaja[x] = 0;
+    }
+    for(int x = 0; x < K_MAX_CajaExclusive; x++)
+    {
+        lCajaEx[x] = 0;
+    }
+
+
+    do
+    {
+        //--Generador de personas
+        if(lPersonas < K_MAX_Personas)
+        {
+            lPersonas++;
+            lOpers = random(1,K_MAX_Opers,distribution);
+            //lOpers = 1;
+            cout << "#Opers: " << lOpers  << ", #Pers: " << lPersonas << endl;
+            //--Ingresar en la cola
+            if(lOpers == 1)
+            {
+                if (!lColaExclusive.isFull())
+                {
+                    lColaExclusive.push(lOpers);
+
+                } else { 
+                    lRebotes++;
+                }
+                
+            }
+            if(lOpers != 1){
+                if (!lCola.isFull())
+                {
+                    lCola.push(lOpers);
+
+                } else { 
+                    if(lColaExclusive.isEmpty())
+                    {
+                        lColaExclusive.push(lOpers);
+                    }
+                    else{
+                        lRebotes++;
+                    }
+                }
+
+            }
+        }
+
+        //--Pedir gente y hacer operaciones
+        lCola.repr();
+        lColaExclusive.repr();
+        lCajaVacia = 0;
+        lCajaExVacia = 0;
+
+        
+        cout << "Caja #Exc : " << lCajaEx[0] << endl;
+
+        if(lCajaEx[0] != 0){
+            lCajaEx[0]--;
+            }
+        else{
+            if(!lColaExclusive.isEmpty()){
+                lCajaEx[0] = lColaExclusive.top();
+                lColaExclusive.pop();
+            } 
+            else{ lCajaExVacia++;}
+            }
+        cout << "," << lCajaEx[0] << endl;
+        
+
+        for(int x = 0; x < K_MAX_Cajas; x++)
+        {   
+
+            cout << "Caja #" << x << ": " << lCaja[x] << endl;
+            if(lCaja[x] != 0){
+                lCaja[x]--;
+            }
+            else{
+                if(!lCola.isEmpty()){
+                    lCaja[x] = lCola.top();
+                    lCola.pop();
+                } 
+                else{ lCajaVacia++;}
+            }
+            cout << "," << lCaja[x] << endl;
+           
+        }
+
+        
+
+        lCola.repr();
+        lColaExclusive.repr();
+
+        //--Determinar el fin de la simulación
+        lSalida = 
+            (lPersonas == K_MAX_Personas) &&
+            (lCola.isEmpty()) &&
+            (lColaExclusive.isEmpty()) &&
+            (lCajaVacia == K_MAX_Cajas) &&
+            (lCajaExVacia == K_MAX_CajaExclusive);
+        //1. No más personas, 2. Que no haya personas en la cola, 3. No personas en caja
+
+        lCiclos++;
+        cout << "------" << lCiclos << "-----" << endl;
+
+    } while (!lSalida);
+
+    cout << "Max Personas: " << K_MAX_Personas << endl;
+    cout << "Max Lugares: " << K_MAX_Lugares << endl;
+    cout << "Max Cajas: " << K_MAX_Cajas << endl;
+    cout << "Max Cajas Exlusivas: " << K_MAX_CajaExclusive << endl;
+    cout << "Max Opers: " << K_MAX_Opers << endl;
+    cout << "Ciclos: " << lCiclos << endl;
+    cout << "Rebotes: " << lRebotes << endl;
+
+
+    /*Max Personas: 1000
+    Max Lugares: 10
+    Max Cajas: 2
+    Max Cajas Exlusivas: 1
+    Max Opers: 6
+    Ciclos: 1023
+    Rebotes: 107*/
+
+}
--- lista.cpp
+++ lista.cpp
@@ -5,12 +5,148 @@ using namespace std;
 
 typedef struct DNODE* PDNODE;
 
-typedef struct DNODE{
+struct DNODE{
 
     string sNombre;
     int sFrec;
     PDNODE sNext;
     PDNODE sPrev;
 
-
 };
+
+class Dlist{
+
+protected:
+    PDNODE aHead;
+    PDNODE aTail;
+
+public:
+    Dlist(void);
+    ~Dlist(void);
+
+    void clean(void); //Liberar de memoria todos los nodos
+    void push_front(string pNombre);
+    void push_back(string pNombre);
+    void repr(void);
+    void push(string pNombre);
+
+private:
+    PDNODE getNewNode(string pNombre);
+    PDNODE search(string pNombre);
+};
+
+
+Dlist::Dlist(void){
+    aHead = NULL;
+    aTail = NULL;
+}
+Dlist::~Dlist(void){
+    clean();
+}
+void Dlist::clean(void){
+
+    while(aHead){
+        PDNODE lTemp = aHead->sNext;
+        delete aHead;
+        aHead = lTemp;
+    }
+
+    aHead = NULL;
+    aTail = NULL;
+}
+void Dlist::push_front(string pNombre){
+    //Si no hay nodos
+    if(aHead == NULL){
+        aHead = getNewNode(pNombre);
+        aTail = aHead;
+    }
+    else{
+        PDNODE lTemp = getNewNode(pNombre);
+        lTemp->sNext = aHead;
+        aHead->sPrev = lTemp;
+        aHead = lTemp;
+    }
+}
+void Dlist::push_back(string pNombre){
+
+    if(aHead == NULL){
+        aHead = getNewNode(pNombre);
+        aTail = aHead;
+    }
+    else{
+        PDNODE lTemp = getNewNode(pNombre);
+        aTail->sNext = lTemp;
+        lTemp->sPrev = aTail;
+        aTail = lTemp;
+    }
+}
+void Dlist::repr(void){
+    if(aHead){
+        PDNODE lTemp = aHead;
+        while (lTemp)
+        {
+            cout << "-> ";
+            cout << lTemp->sNombre;
+            lTemp = lTemp->sNext;
+        }
+        cout << "->|| " << endl;
+        
+    }
+}
+
+PDNODE Dlist::search(string pNombre){
+
+    PDNODE lTemp = aHead;
+    while(!lTemp){
+        if(pNombre <= lTemp->sNombre){
+            return lTemp;
+        }
+        lTemp = lTemp->sNext;
+    }
+
+    return NULL;
+}
+
+PDNODE Dlist::getNewNode(string pNombre){
+    PDNODE lTemp = new DNODE;
+    if(lTemp){
+        lTemp->sNombre = pNombre;
+        lTemp->sFrec = 1;
+        lTemp->sNext = NULL;
+        lTemp->sPrev = NULL;
+    }
+
+    return lTemp;
+}
+
+void Dlist::push(string pNombre){
+    if(aHead == NULL){
+        aHead = getNewNode(pNombre);
+        aTail = aHead;
+    }
+    else{
+        if(pNombre <= aHead->sNombre){
+            push_front(pNombre);
+        }
+        else
+            if(pNombre >= aTail->sNombre){
+                push_back(pNombre);
+            }
+            else{
+                PDNODE lTemp = search(pNombre);
+                //Aqui 
+            }
+    }
+}
+
+int main()
+{
+    Dlist lLista = Dlist();
+
+    lLista.push_front("Peter Parker");
+    lLista.push_back("Clark Kent");
+    lLista.push_front("Diana Prince");
+
+    lLista.repr();
+
+}
